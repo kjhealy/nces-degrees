843210adc39cf32b9bf1cd11c87541467990e6ca
Kieran Healy <kjhealy@gmail.com>
2018-04-04 14:37:42 -0400

    two more trend plots

--- nces.r
+++ nces.r
@@ -61,3 +61,53 @@ p + geom_line() +
                   caption = "Data from NCES Digest 2017, Table 322.10.") +
     theme_minimal() +
     theme(strip.text.x = element_text(size = 6, face = "bold"))
+
+
+area_pcts <- data_l %>% group_by(field_of_study) %>%
+    summarize(mean_pct = mean(yr_pct)) %>%
+    mutate(cutoff = mean_pct < 2)
+
+area_pcts %>% arrange(desc(mean_pct)) %>% data.frame()
+
+data_l <- data_l %>% mutate(field_sum = field_of_study)
+
+ind <- area_pcts$field_of_study[!area_pcts$cutoff]
+
+data_l$cutoff <- data_l$field_of_study %in% ind
+
+
+p <- ggplot(subset(data_l, cutoff == FALSE),
+            aes(x = yr,
+                y = yr_pct,
+                group = field_of_study))
+
+p + geom_line() +
+    facet_wrap(~ reorder(field_of_study, -yr_pct),
+               labeller = label_wrap_gen(width = 35),
+               ncol = 5) +
+    labs(x = "Year",
+         y = "Percent of all BAs conferred",
+         caption = "Data from NCES Digest 2017, Table 322.10.",
+         title = "US Trends in Bachelor's Degrees Conferred, 1970-2015,\nfor Areas averaging less than 2% of all degrees",
+         subtitle = "Observations are every 5 years from 1970-1995, and annually thereafter") +
+    theme_minimal() +
+    theme(strip.text.x = element_text(size = 6))
+
+
+
+p <- ggplot(subset(data_l, cutoff == TRUE),
+            aes(x = yr,
+                y = yr_pct,
+                group = field_of_study))
+
+p + geom_line() +
+    facet_wrap(~ reorder(field_of_study, -yr_pct),
+               labeller = label_wrap_gen(width = 35),
+               ncol = 5) +
+    labs(x = "Year",
+         y = "Percent of all BAs conferred",
+         caption = "Data from NCES Digest 2017, Table 322.10.",
+         title = "US Trends in Bachelor's Degrees Conferred, 1970-2015,\nfor Areas averaging more than 2% of all degrees",
+         subtitle = "Observations are every 5 years from 1970-1995, and annually thereafter") +
+    theme_minimal() +
+    theme(strip.text.x = element_text(size = 6))
